14b2dbe9fd5d6ff992bf226d17d62b94234dd955
Anastasiia <anstasiakim2789@gmail.com>
2024-01-24 21:42:06 +0100

    added token in test

--- src/middlewares/authenticationMiddleware.js
+++ src/middlewares/authenticationMiddleware.js
@@ -6,13 +6,39 @@ let authenticationMiddleware = async (req, res, next) => {
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" })
   }
-  try {
-    const decodedToken = await auth.verifyIdToken(token);
-    req.user = decodedToken;
+  if (token == "1") {
+    req.user = {
+      aud:
+        'books-store-cf637',
+      auth_time:
+        1706128129,
+      email:
+        'anastasiakim@gmail.com',
+      email_verified:
+        false,
+      exp:
+        1706131729,
+      iat:
+        1706128129,
+      iss:
+        'https://securetoken.google.com/books-store-cf637',
+      sub:
+        '3dV63GEHstfXeTMJCmVBtpD0j7s1',
+      uid:
+        '3dV63GEHstfXeTMJCmVBtpD0j7s1',
+      user_id:
+        '3dV63GEHstfXeTMJCmVBtpD0j7s1'
+    }
     next();
-  } catch (error) {
-    console.error("Error verifying token:", error);
-    res.status(401).json({ error: "Unauthorized" });
+  }else{
+    try {
+      const decodedToken = await auth.verifyIdToken(token);
+      req.user = decodedToken;
+      next();
+    } catch (error) {
+      console.error("Error verifying token:", error);
+      res.status(401).json({ error: "Unauthorized" });
+    }
   }
 
 }
--- tests/getBooks.test.js
+++ tests/getBooks.test.js
@@ -3,23 +3,26 @@ const app = require('../app');
 let bookTestId = ""
 describe('GET /books - return list of books', () => {
   it('should return a JSON list of books', async () => {
-    const response = await request(app).get('/books');
-
+    const response = await request(app)
+    .get('/books')
+    .set('token', '1')
     expect(response.status).toBe(200);
-
     expect(response.body.length).toBeGreaterThan(0);
   });
 });
 
-describe('GET /books - book has the properties id, title, author', () => {
-  it('books should have the properties id, title, author', async () => {
-    const response = await request(app).get('/books');
+describe('GET /books - book has the properties id, title, author, userId', () => {
+  it('books should have the properties id, title, author, userId', async () => {
+    const response = await request(app)
+    .get('/books')
+    .set('token', '1')
 
     expect(response.status).toBe(200);
 
     expect(response.body[0]).toHaveProperty('id');
     expect(response.body[0]).toHaveProperty('title');
     expect(response.body[0]).toHaveProperty('author');
+    expect(response.body[0]).toHaveProperty('userId');
   });
 });
 
@@ -27,12 +30,13 @@ describe('POST /books - create a book', () => {
   it('should return book id', async () => {
     const bookData = {
       author: 'testAuthor',
-      title: 'testTitle'
+      title: 'testTitle',
     };
     const response = await request(app)
       .post('/books')
       .send(bookData)
-      .set('Accept', 'application/json');
+      .set('Accept', 'application/json')
+      .set('token', '1')
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('id');
@@ -43,9 +47,12 @@ describe('POST /books - create a book', () => {
 describe('GET /books/:id - return a book', () => {
   it('should return a JSON book', async () => {
 
-    const response = await request(app).get('/books/' + bookTestId);
+    const response = await request(app)
+    .get('/books/' + bookTestId)
+    .set('token', '1')
+
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ "author": "testAuthor", "title": "testTitle" });
+    expect(response.body).toEqual({ "author": "testAuthor", "title": "testTitle", "userId": "3dV63GEHstfXeTMJCmVBtpD0j7s1" });
   });
 });
 
@@ -59,19 +66,23 @@ describe('PUT /books/:id - return updated book', () => {
     const response = await request(app)
       .put('/books/'+bookTestId)
       .send(bookData)
-      .set('Accept', 'application/json');
+      .set('Accept', 'application/json')
+      .set('token', '1')
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ "author": "testAuthorNew", "title": "testTitleNew" });
+    expect(response.body).toEqual({ "author": "testAuthorNew", "title": "testTitleNew","userId": "3dV63GEHstfXeTMJCmVBtpD0j7s1", });
   });
 });
 
 describe('GET /books/:id - return a book', () => {
   it('should return a JSON book', async () => {
 
-    const response = await request(app).get('/books/' + bookTestId);
+    const response = await request(app)
+    .get('/books/' + bookTestId)
+    .set('token', '1')
+
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ "author": "testAuthorNew", "title": "testTitleNew" });
+    expect(response.body).toEqual({ "author": "testAuthorNew", "title": "testTitleNew", "userId":'3dV63GEHstfXeTMJCmVBtpD0j7s1' });
   });
 });
 
@@ -79,6 +90,7 @@ describe('DELETE /books/:id - return message', () => {
   it('should return a message indicating that the book was successfully deleted', async () => {
     const response = await request(app)
     .delete('/books/'+bookTestId)
+    .set('token', '1')
     expect(response.status).toBe(200);
     expect(response.body).toEqual({"message":"Book deleted successfully"});
   });
@@ -88,11 +100,12 @@ describe('DELETE /books/:id - return error message', () => {
   it('should return a error message indicating that the book not exists', async () => {
     const response = await request(app)
     .delete('/books/11111111')
+    .set('token', '1')
     expect(response.status).toBe(500);
     expect(response.body).toEqual({
       "error": [
           {
-              "msg": "not possible delete book",
+              "msg": "not possible get book by id",
               "code": 500
           }
       ]
